08ad7f29f8219ae3bd247aae4d75700342a0f7bb
Uladzislau Bondar <vladislav.bondar@itechart-group.com>
2018-11-27 19:47:47 +0300

    fix getUsers function

--- services/userService.js
+++ services/userService.js
@@ -5,9 +5,25 @@ const connect = parse('postgres://IvanZharnov:0987654321@localhost:5432/usersDB'
 const pool = new Pool(connect);
 
 exports.getUsersService = () => {
-  pool.connect((err, client, done) => {
-    client.query('SELECT * FROM users ORDER BY id ASC')
-    .then(res => console.log(res.rows))
-    .catch(err => console.error(err.stack))
+  return new Promise((resolve, reject) => {
+    pool.connect( async (err, client) => {
+      const result = await client.query('SELECT * FROM users ORDER BY id ASC');
+      if (err) {
+        return console.error('Error acquiring client', err.stack)
+      };
+      console.log('service', result.rows);
+      return result.rows;
+    });
   });
 };
+
+// exports.createUserService = (data) => {
+//   pool.connect(async(err, client, done) => {
+//     const newUser = await client.query('INSERT INTO users(first_name, last_name) values($1, $2) RETURNING *', [data.first_name, data.last_name]);
+//     if(err) {
+//       console.log('error fetching client from pull', err);
+//     }
+//     console.log('service', newUser.rows);
+//     return newUser.rows;
+//   });
+// };
