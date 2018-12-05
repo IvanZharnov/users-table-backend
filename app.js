5f54ca8625d4f9fe27aceec4a71892d32d18d8fa
Ivan Zharnov <ivan.zharnov@gmail.com>
2018-12-05 15:05:57 +0300

    fix connection config

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const users = require('./routes/users')
+const users = require('./routes/routes')
 
 const app = express();
 
